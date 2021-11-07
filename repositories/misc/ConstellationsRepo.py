a3d7efef6dd322f7294d873f400ba81ee9b33f7b
BigCoight <ajzulli@gmail.com>
2021-11-07 20:59:04 +1100

    Cleaning up sludgers mess

--- repositories/misc/ConstellationsRepo.py
+++ repositories/misc/ConstellationsRepo.py
@@ -1,9 +1,9 @@
 from typing import List
+
 from definitions.misc.Constellation import Constellation
-from definitions.misc.MapName import MapName
-from repositories.misc.MapNameRepo import MapNameRepo
-from helpers.HelperFunctions import getFromArrayArray, getFromSplit, getFromSplitArray
+from helpers.HelperFunctions import getFromSplitArray
 from repositories.master.Repository import Repository
+from repositories.misc.MapNameRepo import MapNameRepo
 
 
 class ConstellationsRepo(Repository[Constellation]):
@@ -15,34 +15,36 @@ class ConstellationsRepo(Repository[Constellation]):
 	def initDependencies(cls):
 		MapNameRepo.initialise(cls.codeReader)
 
-
 	@classmethod
 	def getSections(cls) -> List[str]:
 		return ["Constellations"]
 
 	@classmethod
 	def generateRepo(cls) -> None:
+		prefixes = ["A", "B", "C"]
 		# StarQuests
 		data = getFromSplitArray(cls.getSection())
 		prefix = 'A'
 		emptyCount = 0
 		perWorldCounter = 1
-		for constellation in data:
-			# Count the empty rows and skip them
+		for n, constellation in enumerate(data):
+			prefIndex = n // 12
+			# # Count the empty rows and skip them
 			if constellation[0] == '':
-				emptyCount += 1
 				continue
-
-			# After 2 empty rows, world 2 constellations start
-			if emptyCount == 2 and prefix != 'B':
-				prefix = 'B'
-				perWorldCounter = 1
-			# After 3 more empty rows, world 3 constellations will start (future proofing)
-			elif emptyCount == 5 and prefix != 'C':
-				prefix = 'C'
-				perWorldCounter = 1
+			# 	continue
+			#
+			# # After 2 empty rows, world 2 constellations start
+			# if emptyCount == 2 and prefix != 'B':
+			# 	prefix = 'B'
+			# 	perWorldCounter = 1
+			# # After 3 more empty rows, world 3 constellations will start (future proofing)
+			# elif emptyCount == 5 and prefix != 'C':
+			# 	prefix = 'C'
+			# 	perWorldCounter = 1
+			prefix = prefixes[prefIndex]
 
 			mapName = MapNameRepo.get(int(constellation[0])).name
-			finalData = [ f"{prefix}-{perWorldCounter}", mapName, *constellation[1:] ]
-			cls.add(f"{prefix}-{perWorldCounter}", Constellation.fromList(finalData))
-			perWorldCounter += 1		
+			finalData = [f"{prefix}-{(n % 12) + 1}", mapName, *constellation[1:]]
+			cls.add(f"{prefix}-{(n % 12) + 1}", Constellation.fromList(finalData))
+			perWorldCounter += 1

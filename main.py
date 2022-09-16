05251d20c47991f0aafc64737322b44f099b129e
BigCoight <ajzulli@gmail.com>
2022-09-17 06:11:30 +1000

    Updating to is update

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ from repositories.npc.QuestNameRepo import QuestNameRepo
 from repositories.talents.TalentNameRepo import TalentNameRepo
 from repositories.talents.TalentTreeRepo import TalentTreeRepo
 
-idleonReader = IdleonReader("-is", steam = True)
+idleonReader = IdleonReader("161is", steam = True)
 # idleonReader = IdleonReader("123b", False)
 codeReader = idleonReader.codeReader
 CustomSourceRepo.initialise(codeReader)
@@ -160,3 +160,7 @@ SkullShopRepo.initialise(codeReader)
 
 RandoListRepo.initialise(codeReader)
 ColosseumRepo.initialise(codeReader)
+
+ItemRepo.exportWikiMult()
+NpcRepo.exportWikiMult()
+EnemyRepo.exportWikiMult()

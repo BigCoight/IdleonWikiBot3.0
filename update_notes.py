de8f316dda18af03727bb7d46dddfddfb3c29a9c
BigCoight <ajzulli@gmail.com>
2024-04-27 10:33:43 +1000

    206

--- update_notes.py
+++ update_notes.py
@@ -1,10 +1,13 @@
 from helpers.CodeReader import IdleonReader
+from repositories.item.sources.ItemNoteRepo import ItemNoteRepo
+from repositories.item.sources.WikiSetRepo import WikiSetRepo
+from repositories.npc.NPCNoteRepo import NpcNoteRepo
 from repositories.npc.NpcHeadRepo import NpcHeadRepo
 
-idleonReader = IdleonReader("204", steam = True)
+idleonReader = IdleonReader("206", steam = True)
 codeReader = idleonReader.codeReader
 
-# WikiSetRepo.initialise(codeReader, True)
-# ItemNoteRepo.initialise(codeReader, True)
+WikiSetRepo.initialise(codeReader, True)
+ItemNoteRepo.initialise(codeReader, True)
 NpcHeadRepo.initialise(codeReader, True)
-# NpcNoteRepo.initialise(codeReader, True)
+NpcNoteRepo.initialise(codeReader, True)
